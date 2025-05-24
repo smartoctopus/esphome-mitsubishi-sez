23a02fc0218b20b59d84636924ad221c835f2132
smartoctopus <89800664+smartoctopus@users.noreply.github.com>
2025-05-24 12:43:38 +0200

    Fix climate spelling

--- esphome/components/mitsubishi_sez/climate.py
+++ esphome/components/mitsubishi_sez/climate.py
@@ -6,7 +6,7 @@ AUTO_LOAD = ['climate_ir']
 mitsubishi_sez_ns = cg.esphome_ns.namespace('mitsubishi_sez')
 MitsubishiSEZClimate = mitsubishi_sez_ns.class_('MitsubishiSEZClimate', climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climare_ir_with_receiver_schema(MitsubishiSEZClimate)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(MitsubishiSEZClimate)
 
 
 async def to_code(config):
