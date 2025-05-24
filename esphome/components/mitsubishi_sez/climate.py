ce0a6d64c86bf6405a8d32861b478248979a7b79
smartoctopus <89800664+smartoctopus@users.noreply.github.com>
2025-05-24 12:35:41 +0200

    Update climate.py to use climate_schema(...)

--- esphome/components/mitsubishi_sez/climate.py
+++ esphome/components/mitsubishi_sez/climate.py
@@ -1,18 +1,13 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID
 
 AUTO_LOAD = ['climate_ir']
 
 mitsubishi_sez_ns = cg.esphome_ns.namespace('mitsubishi_sez')
 MitsubishiSEZClimate = mitsubishi_sez_ns.class_('MitsubishiSEZClimate', climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(MitsubishiSEZClimate),
-})
+CONFIG_SCHEMA = climate_ir.climare_ir_with_receiver_schema(MitsubishiSEZClimate)
 
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield climate_ir.register_climate_ir(var, config)
+    await climate_ir.new_climate_ir(config)
